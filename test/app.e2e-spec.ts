645a590854c80e0f7f73e1d5aedf5e3d8c480a1a
DMQQ <kozakdamian73@gmail.com>
2021-11-10 14:43:22 +0100

    enable cors

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,6 @@
-import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import * as request from "supertest";
+import { Test } from "@nestjs/testing";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+import { INestApplication } from "@nestjs/common";
 
-  beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+describe("AppController (e2e)", () => {});
